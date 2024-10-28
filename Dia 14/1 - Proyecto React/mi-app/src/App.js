7e57c46369598c3f69b076625ace544b480f7126
ejoelrojas9 <96266652+ejoelrojas9@users.noreply.github.com>
2024-10-28 18:03:09 -0500

    Creando y modificando complementos

--- Dia 14/1 - Proyecto React/mi-app/src/App.js
+++ Dia 14/1 - Proyecto React/mi-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(props) {
+  let [contador, setearConteo] = React.useState(0);
+
+  function aumentarConteo() {
+    setearConteo(contador + 1);
+  }
+
+  return(
+    <div style={{backgroundColor: props.colorFondo}}>
+      <h1 style={{color: props.colorTexto}}>Contador: {contador}</h1>
+      <button onClick={aumentarConteo}>Incrementar</button>
     </div>
-  );
+  )
 }
 
 export default App;
